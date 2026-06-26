af4b8bf3c5a02f1916771ca7aed213923230cb08
monteiroa <monteiroa@acad.ifma.edu.br>
2026-06-26 11:56:09 -0300

    Update solucao.py

--- solucao.py
+++ solucao.py
@@ -1,5 +1,8 @@
+# Lê as três idades informadas pelo usuário
 a = int(input())
 b = int(input())
 c = int(input())
 
+# Soma as três idades e subtrai a maior e a menor
+# O valor restante é a idade intermediária (de Camila)
 print(a + b + c - max(a, b, c) - min(a, b, c))
